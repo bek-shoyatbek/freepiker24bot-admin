f01843c18b8ae8685b33dab13e5bb2dd01f4bc8d
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-01-18 12:25:07 +0500

    feat: analytics page beautified

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import { API_BASE_URL } from "../constants";
 
 export const Axios = axios.create({
-  // baseURL: API_BASE_URL,
-  baseURL: "http://localhost:5500",
+  baseURL: API_BASE_URL,
   timeout: 10 * 1000,
 });
